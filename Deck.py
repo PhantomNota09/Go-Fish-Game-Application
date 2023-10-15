8f3b53f5856dad1bb678026ba51679e49ffa7d22
Vinay Edupuganti <53072536+vinayedupuganti@users.noreply.github.com>
2023-10-15 10:47:52 -0700

    Update Deck.py
    Added a main method to test the functionality of the DECK object and for debugging purposes

--- Deck.py
+++ Deck.py
@@ -21,3 +21,9 @@ class Deck:
         if 'name' in obj and 'ipv4' in obj and 'mport' in obj and 'rport' in obj and 'pport' in obj:
             return Player(obj['name'], obj['ipv4'],obj['mport'],obj['rport'],obj['pport'])
         return obj
+if __name__ == '__main__':
+    deck = Deck();
+    players = "[{\"name\": \"client2\", \"ipv4\": \"192.168.0.4\", \"mport\": \"15561\", \"rport\": \"15562\", \"pport\": \"15563\"}, {\"name\": \"client1\", \"ipv4\": \"192.168.0.4\", \"mport\": \"15551\", \"rport\": \"15552\", \"pport\": \"15553\"}]"
+    players = json.loads(players, object_hook=deck.player_decoder)
+    print(players)
+    deck.deal_cards(players);
